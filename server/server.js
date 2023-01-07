4c42b4c96505a499c9fb72a5ca6eb4dfc09f044c
sravanthbaratam <sravanthbaratam@gmail.com>
2023-01-07 23:19:24 +0530

    removed html-pdf and used puppeteer for pdf generation.

--- server/server.js
+++ server/server.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const pdf = require("html-pdf");
+// const pdf = require("html-pdf");
 const path = require("path");
 
 const app = express();
 
 const pdfTemplate = require("./documents");
+const puppeteer = require('puppeteer');
 
-const options = {
-	height: "42cm",
-	width: "29.7cm",
-	timeout: "6000",
-};
+// const options = {
+// 	height: "42cm",
+// 	width: "29.7cm",
+// 	timeout: "6000",
+// };
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -23,13 +24,47 @@ app.use(express.static(path.join(__dirname, "/client/build")));
 // });
 
 // POST route for PDF generation....
-app.post("/create-pdf", (req, res) => {
-	pdf.create(pdfTemplate(req.body), options).toFile("Resume.pdf", (err) => {
-		if (err) {
-			console.log(err);
-			res.send(Promise.reject());
-		} else res.send(Promise.resolve());
+app.post("/create-pdf", async (req, res) => {
+	// pdf.create(pdfTemplate(req.body), options).toFile("Resume.pdf", (err) => {
+	// 	if (err) {
+	// 		console.log(err);
+	// 		res.send(Promise.reject());
+	// 	} else res.send(Promise.resolve());
+	// });
+
+	// Create a browser instance
+	const browser = await puppeteer.launch();
+
+	// Create a new page
+	const page = await browser.newPage();
+
+	// // Website URL to export as pdf
+	// const website_url = 'https://www.bannerbear.com/blog/how-to-download-images-from-a-website-using-puppeteer/';
+
+	// // Open URL in current page
+	// await page.goto(website_url, { waitUntil: 'networkidle0' });
+
+	//Get HTML content from HTML file
+	const html = pdfTemplate(req.body)
+	//fs.readFileSync('sample.html', 'utf-8');
+	await page.setContent(html, { waitUntil: 'domcontentloaded' });
+
+	//To reflect CSS used for screens instead of print
+	await page.emulateMediaType('screen');
+
+	// Downlaod the PDF
+	const pdf = await page.pdf({
+		path: path.join(__dirname, `Resume.pdf`),
+		margin: { top: '100px', right: '50px', bottom: '100px', left: '50px' },
+		printBackground: true,
+		format: 'A4',
 	});
+
+	// Close the browser instance
+	await browser.close();
+
+	res.send(Promise.resolve())
+
 });
 
 // GET route -> send generated PDF to client...
