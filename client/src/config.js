c971d6f3ba624ee72fccf5ad50dcfbec694a6c08
sravanthbaratam <sravanthbaratam@gmail.com>
2023-01-08 12:51:54 +0530

    Added puppeteer config.

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/"
+  baseURL: "https://resume-builder-api.onrender.com"
+  //"http://localhost:5000/"
 });
